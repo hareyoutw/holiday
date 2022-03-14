b7b8894ce2793950061bb2bf3f65fdc87ae2abac
hareyoutw <80091936+hareyoutw@users.noreply.github.com>
2022-03-14 22:52:49 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+
+//just font
+var _jf = _jf || [];_jf.push(['p','57530']);_jf.push(['initAction',true]);_jf.push(['_setFont','datk6','css','.datk6']);_jf.push(['_setFont','datk6','alias','datk6']);_jf.push(['_setFont','datk6','english','Courgette']);_jf.push(['_setFont','datk6','weight',600]);_jf.push(['_setFont','datk7','css','.datk7']);_jf.push(['_setFont','datk7','alias','datk7']);_jf.push(['_setFont','datk7','english','Vollkorn']);_jf.push(['_setFont','datk7','weight',600]);_jf.push(['_setFont','datk8','css','.datk8']);_jf.push(['_setFont','datk8','alias','datk8']);_jf.push(['_setFont','datk8','english','Gentium Book Basic']);_jf.push(['_setFont','datk8','weight',700]);_jf.push(['_setFont','datm8','css','.datm8']);_jf.push(['_setFont','datm8','alias','datm8']);_jf.push(['_setFont','datm8','english','Volkhov']);_jf.push(['_setFont','datm8','weight',800]);_jf.push(['_setFont','datw5','css','.datw5']);_jf.push(['_setFont','datw5','alias','datw5']);_jf.push(['_setFont','datw5','english','Alegreya']);_jf.push(['_setFont','datw5','weight',600]);_jf.push(['_setFont','daty3','css','.daty3']);_jf.push(['_setFont','daty3','alias','daty3']);_jf.push(['_setFont','daty3','english','Source Sans Pro']);_jf.push(['_setFont','daty3','weight',400]);_jf.push(['_setFont','daty5','css','.daty5']);_jf.push(['_setFont','daty5','alias','daty5']);_jf.push(['_setFont','daty5','english','Source Sans Pro']);_jf.push(['_setFont','daty5','weight',600]);_jf.push(['_setFont','daty7','css','.daty7']);_jf.push(['_setFont','daty7','alias','daty7']);_jf.push(['_setFont','daty7','english','Varela Round']);_jf.push(['_setFont','daty7','weight',700]);_jf.push(['_setFont','daty9','css','.daty9']);_jf.push(['_setFont','daty9','alias','daty9']);_jf.push(['_setFont','daty9','english','Nunito']);_jf.push(['_setFont','daty9','weight',800]);_jf.push(['_setFont','hanamin','css','.hanamin']);_jf.push(['_setFont','hanamin','alias','hanamin']);_jf.push(['_setFont','hanamin','english','Lora']);_jf.push(['_setFont','hanamin','weight',400]);_jf.push(['_setFont','chekiangshukesung','css','.chekiangshukesung']);_jf.push(['_setFont','chekiangshukesung','alias','chekiangshukesung']);_jf.push(['_setFont','chekiangshukesung','english','IM Fell French Canon']);_jf.push(['_setFont','chekiangshukesung','weight',700]);_jf.push(['_setFont','setofont','css','.setofont']);_jf.push(['_setFont','setofont','alias','setofont']);_jf.push(['_setFont','setofont','weight',600]);_jf.push(['_setFont','pentouch-regular','css','.pentouch-regular']);_jf.push(['_setFont','pentouch-regular','alias','pentouch-regular']);_jf.push(['_setFont','pentouch-regular','weight',600]);_jf.push(['_setFont','sourcehansans-tc-extralight','css','.sourcehansans-tc-extralight']);_jf.push(['_setFont','sourcehansans-tc-extralight','alias','sourcehansans-tc']);_jf.push(['_setFont','sourcehansans-tc-extralight','weight',100]);_jf.push(['_setFont','sourcehansans-tc-normal','css','.sourcehansans-tc-normal']);_jf.push(['_setFont','sourcehansans-tc-normal','alias','sourcehansans-tc']);_jf.push(['_setFont','sourcehansans-tc-normal','weight',300]);_jf.push(['_setFont','sourcehansans-tc-regular','css','.sourcehansans-tc-regular']);_jf.push(['_setFont','sourcehansans-tc-regular','alias','sourcehansans-tc']);_jf.push(['_setFont','sourcehansans-tc-regular','weight',400]);_jf.push(['_setFont','sourcehansans-tc-medium','css','.sourcehansans-tc-medium']);_jf.push(['_setFont','sourcehansans-tc-medium','alias','sourcehansans-tc']);_jf.push(['_setFont','sourcehansans-tc-medium','weight',600]);_jf.push(['_setFont','sourcehansans-tc-bold','css','.sourcehansans-tc-bold']);_jf.push(['_setFont','sourcehansans-tc-bold','alias','sourcehansans-tc']);_jf.push(['_setFont','sourcehansans-tc-bold','weight',700]);_jf.push(['_setFont','sourcehansans-tc-heavy','css','.sourcehansans-tc-heavy']);_jf.push(['_setFont','sourcehansans-tc-heavy','alias','sourcehansans-tc']);_jf.push(['_setFont','sourcehansans-tc-heavy','weight',900]);_jf.push(['_setFont','sourcehansans-sc-light','css','.sourcehansans-sc-light']);_jf.push(['_setFont','sourcehansans-sc-light','alias','sourcehansans-sc']);_jf.push(['_setFont','sourcehansans-sc-light','weight',300]);_jf.push(['_setFont','sourcehansans-sc-regular','css','.sourcehansans-sc-regular']);_jf.push(['_setFont','sourcehansans-sc-regular','alias','sourcehansans-sc']);_jf.push(['_setFont','sourcehansans-sc-regular','weight',400]);_jf.push(['_setFont','sourcehansans-sc-bold','css','.sourcehansans-sc-bold']);_jf.push(['_setFont','sourcehansans-sc-bold','alias','sourcehansans-sc']);_jf.push(['_setFont','sourcehansans-sc-bold','weight',700]);_jf.push(['_setFont','sourcehansans-sc-heavy','css','.sourcehansans-sc-heavy']);_jf.push(['_setFont','sourcehansans-sc-heavy','alias','sourcehansans-sc']);_jf.push(['_setFont','sourcehansans-sc-heavy','weight',900]);_jf.push(['_setFont','jf-sunday','css','.jf-sunday']);_jf.push(['_setFont','jf-sunday','alias','jf-sunday']);_jf.push(['_setFont','jf-sunday','weight',600]);_jf.push(['_setFont','jf-justwriting','css','.jf-justwriting']);_jf.push(['_setFont','jf-justwriting','alias','jf-justwriting']);_jf.push(['_setFont','jf-justwriting','weight',500]);_jf.push(['_setFont','mei-a','css','.mei-a']);_jf.push(['_setFont','mei-a','alias','mei-a']);_jf.push(['_setFont','mei-a','weight',600]);_jf.push(['_setFont','oyeh_handwriting','css','.oyeh_handwriting']);_jf.push(['_setFont','oyeh_handwriting','alias','oyeh_handwriting']);_jf.push(['_setFont','oyeh_handwriting','weight',600]);_jf.push(['_setFont','xionggu','css','.xionggu']);_jf.push(['_setFont','xionggu','alias','xionggu']);_jf.push(['_setFont','xionggu','weight',600]);_jf.push(['_setFont','jf-jinxuan-medium','css','.jf-jinxuan-medium']);_jf.push(['_setFont','jf-jinxuan-medium','alias','jf-jinxuan']);_jf.push(['_setFont','jf-jinxuan-medium','weight',500]);_jf.push(['_setFont','notoserifcjktc_light','css','.notoserifcjktc_light']);_jf.push(['_setFont','notoserifcjktc_light','alias','notoserifcjktc']);_jf.push(['_setFont','notoserifcjktc_light','weight',200]);_jf.push(['_setFont','notoserifcjktc_medium','css','.notoserifcjktc_medium']);_jf.push(['_setFont','notoserifcjktc_medium','alias','notoserifcjktc']);_jf.push(['_setFont','notoserifcjktc_medium','weight',400]);_jf.push(['_setFont','notoserifcjktc_semibold','css','.notoserifcjktc_semibold']);_jf.push(['_setFont','notoserifcjktc_semibold','alias','notoserifcjktc']);_jf.push(['_setFont','notoserifcjktc_semibold','weight',600]);_jf.push(['_setFont','notoserifcjktc_bold','css','.notoserifcjktc_bold']);_jf.push(['_setFont','notoserifcjktc_bold','alias','notoserifcjktc']);_jf.push(['_setFont','notoserifcjktc_bold','weight',700]);_jf.push(['_setFont','notoserifcjktc_black','css','.notoserifcjktc_black']);_jf.push(['_setFont','notoserifcjktc_black','alias','notoserifcjktc']);_jf.push(['_setFont','notoserifcjktc_black','weight',900]);_jf.push(['_setFont','chungtf','css','.chungtf']);_jf.push(['_setFont','chungtf','alias','chungtf']);_jf.push(['_setFont','chungtf','weight',500]);_jf.push(['_setFont','duoduofont','css','.duoduofont']);_jf.push(['_setFont','duoduofont','alias','duoduofont']);_jf.push(['_setFont','duoduofont','weight',500]);_jf.push(['_setFont','drechifont','css','.drechifont']);_jf.push(['_setFont','drechifont','alias','drechifont']);_jf.push(['_setFont','drechifont','weight',500]);(function(A,p,c,m,l,q,r,h,B,D){var b=A._jf;if(b.constructor!==Object){var t=!0,u=function(a){var f=!0,e;for(e in b)b[e][0]==a&&(f&&(f=f&&!1!==b[e][1].call(b)),b[e]=null,delete b[e])},v=/\S+/g,w=/[\t\r\n\f]/g,C=/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g,n="".trim,x=n&&!n.call("\ufeff\u00a0")?function(a){return null==a?"":n.call(a)}:function(a){return null==a?"":(a+"").replace(C,"")},k=function(a){var f,b,g;if("string"===typeof a&&a&&(a=(a||"").match(v)||[],f=h[c]?(" "+h[c]+" ").replace(w," "):" ")){for(g=0;b=a[g++];)0>f.indexOf(" "+b+" ")&&(f+=b+" ");h[c]=x(f)}},d=function(a){var b,e,g;if(0===arguments.length||"string"===typeof a&&a){var d=(a||"").match(v)||[];if(b=h[c]?(" "+h[c]+" ").replace(w," "):""){for(g=0;e=d[g++];)for(;0<=b.indexOf(" "+e+" ");)b=b.replace(" "+e+" "," ");h[c]=a?x(b):""}}},y;b.addScript=y=function(b,f,e,g,d,c){d=d||function(){};c=c||function(){};var a=p.createElement("script"),h=p.getElementsByTagName("script")[0],k,m=!1,l=function(){a.src="";a.parentNode.removeChild(a);a=a.onerror=a.onload=a.onreadystatechange=null};g&&(k=setTimeout(function(){l();c()},g));a.type=f||"text/javascript";a.async=e;a.onload=a.onreadystatechange=function(b,c){m||a.readyState&&!/loaded|complete/.test(a.readyState)||(m=!0,g&&clearTimeout(k),l(),c||d())};a.onerror=function(a,b,d){g&&clearTimeout(k);l();c();return!0};a.src=b;h.parentNode.insertBefore(a,h)};for(var z in b)"initAction"==b[z][0]&&(t=b[z][1]);b.push(["_eventPreload",function(){1==t&&k(m);y(B,null,!1,3E3,null,function(){u("_eventInactived")})}]);b.push(["_eventReload",function(){d(r);d(q);k(l)}]);b.push(["_eventActived",function(){d(m);d(l);k(q)}]);b.push(["_eventInactived",function(){d(m);d(l);k(r)}]);u("_eventPreload")}})(this,this.document,"className","jf-loading","jf-reloading","jf-active","jf-inactive",this.document.getElementsByTagName("html")[0],"//d3gc6cgx8oosp4.cloudfront.net/js/stable/v/5.0.6/id/194622270375");
+
+//for wow.min.js
+new WOW().init();
+
+
+//niceScroll
+  $("html").niceScroll({
+       autohidemode: false,
+  cursorcolor:"#e0ba60", 
+  cursorwidth: "8", 
+  cursorborder: "",
+       cursoropacitymax:0.6
+      //cursorborderradius: "0px"
+});
+
+
+//Go To Top btn & smooth scroll - refer https://codepen.io/desirecode/pen/MJPJqV
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#btn_GTT').fadeIn(); 
+        } else { 
+            $('#btn_GTT').fadeOut(); 
+        } 
+    }); 
+    $('#btn_GTT').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
+
+
+// nav & smooth scroll - https://byprimer.co/blog/navigation-smooth-scrolling-jquery/
+$(document).ready(function() {
+	
+  var headerHeight = $('nav').outerHeight(); // Target your header navigation here
+	
+  $('#nav_smooth li a').click(function(e) {
+  	
+  	var targetHref   = $(this).attr('href');
+	  
+	$('html, body').animate({
+		scrollTop: $(targetHref).offset().top - headerHeight // Add it to the calculation here
+	}, 1000);
+    
+    e.preventDefault();
+  });
+});
+
+
